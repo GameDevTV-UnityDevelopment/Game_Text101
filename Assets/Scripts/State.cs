750d6df168d693d2506e36a71630910bb78b33ac
Rob Meade <baldy.meade@gmail.com>
2018-10-28 18:58:43 +0000

    Implement remastered 2018 changes

---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    [TextArea(10, 14)]
+    [SerializeField]
+    private string storyText;
+
+    [SerializeField]
+    private State[] nextStates;
+
+
+    public State[] GetNextStates()
+    {
+        return nextStates;
+    }
+
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+}
